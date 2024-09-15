a3e38d4c7a6124479a0f72c1ea0aae1099d7adc6
Neil Franci <66520542+neilfranci@users.noreply.github.com>
2024-09-15 11:58:36 +0700

    refactor!: This is a cleanup before migrating to HTMX RESTFUL
    remove javafx client
    remove multiple project setup root
    now it's server only

---
+++ src/main/java/com/bgsix/bookmanagement/dto/BookResponse.java
@@ -0,0 +1,19 @@
+package com.bgsix.bookmanagement.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookResponse {
+	private List<BookDTO> books;
+	private int totalPages;
+	private long totalElements;
+
+}
---
+++ src/main/java/com/bgsix/bookmanagement/repository/BookGenreRepository.java
@@ -0,0 +1,24 @@
+package com.bgsix.bookmanagement.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.bgsix.bookmanagement.model.Book;
+import com.bgsix.bookmanagement.model.BookGenre;
+import com.bgsix.bookmanagement.model.Genre;
+
+@Repository
+public interface BookGenreRepository extends JpaRepository<BookGenre, Long> {
+
+	@Query("SELECT bg.genre FROM BookGenre bg WHERE bg.book.bookId = :bookId")
+	List<Genre> findGenresByBookId(@Param("bookId") Long bookId);
+
+	@Query("SELECT bg.genre FROM BookGenre bg WHERE bg.book.bookId IN :bookIds")
+	List<Genre> findGenresByBookIds(@Param("bookIds") List<Long> bookIds);
+
+	List<BookGenre> findByBookIn(List<Book> books);
+}
