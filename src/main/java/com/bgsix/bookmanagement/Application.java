e12254ee27450cf880fe0092f8b51403ab4f54df
Neil Franci <66520542+neilfranci@users.noreply.github.com>
2024-10-11 08:10:04 +0700

    add collapse button in detail page

--- src/main/java/com/bgsix/bookmanagement/Application.java
+++ src/main/java/com/bgsix/bookmanagement/Application.java
@@ -17,6 +17,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		Logger.info("Swagger UI: {}", serverUrl + "/swagger-ui/index.html");
+		Logger.info("Server URL: {}", serverUrl);
 	}
 
 }
