9db792ae689e9ed9a2f1c039c96e7c4d77cf23ad
Neil Franci <66520542+neilfranci@users.noreply.github.com>
2024-10-23 20:06:39 +0700

    add ico and webp for optimize image background

--- script/drcrim.cmd
+++ script/drcrim.cmd
@@ -12,19 +12,29 @@ if "%~1"=="" (
 :: Set the password from the argument
 set PGPASSWORD=%~1
 
-:: Drop the database, create the database, and load the SQL file
-dropdb -U postgres book-management
+:: Check if the database exists
+psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'book-management'" | findstr /b "1" >nul
 if %ERRORLEVEL% neq 0 (
-    echo Failed to drop the database
-    exit /b %ERRORLEVEL%
+    echo Database 'book-management' does not exist, skipping drop.
+) else (
+    echo Dropping existing database 'book-management'...
+    dropdb -U postgres book-management
+    if %ERRORLEVEL% neq 0 (
+        echo Failed to drop the database
+        exit /b %ERRORLEVEL%
+    )
 )
 
+:: Create the database
+echo Creating database 'book-management'...
 createdb -U postgres book-management
 if %ERRORLEVEL% neq 0 (
     echo Failed to create the database
     exit /b %ERRORLEVEL%
 )
 
+:: Load the SQL file into the database
+echo Importing SQL file into 'book-management'...
 psql -U postgres -f .\book-management-v12.sql -d book-management
 if %ERRORLEVEL% neq 0 (
     echo Failed to execute the SQL file
